7f96571c07fc444b49e7b0fa138991732eaf3232
Alex Wilson <fonzmp@gmail.com>
2018-05-21 03:04:12 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,7 @@ class Transfer
 
   def execute_transaction
     if self.valid?
+      @status = "complete"
       negative_amount = -(@amount)
       @receiver.deposit(@amount)
       @sender.deposit(negative_amount)
